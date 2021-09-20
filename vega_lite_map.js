3033d5de2afbd8a1a11a110931cb335eb3d13815
Mish <89115340+jdan0003@users.noreply.github.com>
2021-09-20 16:37:01 +1000

    Add files via upload

--- vega_lite_map.js
+++ vega_lite_map.js
@@ -1,4 +1,9 @@
 var vg_1 = "worldmap2018.vg.json";
+var vg_2 = "worldmap2019.vg.json";
+var vg_3 = "worldmapmichelin.vg.json";
 
-vegaEmbed("#worldmap", vg_1).then(function(result) {
-}).catch(console.error);
+vegaEmbed("#worldmap2018", vg_1).then(function(result) {}).catch(console.error);
+
+vegaEmbed("#worldmap2019", vg_2).then(function(result){}).catch(console.error);
+
+vegaEmbed("#worldmapmichelin", vg_3).then(function(result){}).catch(console.error);
